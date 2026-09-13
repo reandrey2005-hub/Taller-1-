d864a64f2eb3c7a87f0a8ea2bc5cea4cf027fe2c
reandrey2005-hub <reandrey.2005@gmail.com>
2026-09-13 19:32:31 +0000

    cambio taller 5

--- taller 5/taller 5 app.py
+++ taller 5/taller 5 app.py
@@ -2,17 +2,31 @@ from sklearn.tree import DecisionTreeClassifier, export_text
 import numpy as np
 
 
-X = np.array([ [60, 0], [45, 1], [30, 0], [100, 1], [20, 1] ])
+X = np.array([
+    [22,  8, 3],   # joven, mucho tiempo online, ya compro antes
+    [25,  7, 2],
+    [45,  1, 0],   # mayor, poco tiempo online, nunca compro
+    [50,  0, 0],
+    [23,  9, 4],
+    [60,  1, 0],
+    [30,  6, 2],
+    [55,  2, 1],
+    [21, 10, 5],
+    [48,  1, 0],
+    [28,  6, 1],
+    [65,  0, 0],
+])
 
-Y = np.array([ 1, 0, 1, 1, 0 ])
+
+Y = np.array([1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0])
 
 
 arbol = DecisionTreeClassifier(max_depth=3)
-arbol.fit(X, Y)  # .fit() es el proceso de entrenamiento matemático
+arbol.fit(X, Y)  # .fit() es el proceso de entrenamiento matematico
 
 
-nombres_variables = ["Ingresos", "Tiene_Deuda"]
+nombres_variables = ["Edad", "Horas_Online", "Compras_Previas"]
 reglas_texto = export_text(arbol, feature_names=nombres_variables)
 
-print("Base de Reglas generada automáticamente:\n")
+print("Base de Reglas generada automaticamente:\n")
 print(reglas_texto)
