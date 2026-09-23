c9eedfaba12e8dca4772364052c6cdca22062d6a
reandrey2005-hub <reandrey.2005@gmail.com>
2026-09-23 16:15:51 +0000

    cambio taller 7

--- taller7/taller7 app.py
+++ taller7/taller7 app.py
@@ -1,21 +1,19 @@
 import numpy as np
 from sklearn.svm import SVC
 
-# 1. Crear el dataset (X = Coordenadas, Y = Etiquetas binarias 0 o 1)
-X = np.array([ [2,2], [3,3], [4,2], [6,6], [7,8], [8,7] ])
-Y = np.array([ 0, 0, 0, 1, 1, 1 ])
 
-# 2. Inicializar SVM con Kernel Lineal
-modelo_svm = SVC(kernel='linear')
+X = np.array([ [2,2], [3,3], [4,2], [6,6], [7,8], [8,7], [5,5] ])
+Y = np.array([ 0, 0, 0, 1, 1, 1, 0 ])
 
-# 3. Entrenar el modelo (Aprender la ecuación del hiperplano)
-modelo_svm.fit(X, Y)
-
-# 4. Extraer los Vectores de Soporte descubiertos por la IA
-vectores = modelo_svm.support_vectors_
-print("Los Vectores de Soporte son:\n", vectores)
-
-# 5. Predicción
 nuevo_punto = np.array([[5, 4]])
-pred = modelo_svm.predict(nuevo_punto)
-print("El punto [5,4] pertenece a la clase:", pred[0])
+
+modelo_lineal = SVC(kernel='linear')
+modelo_lineal.fit(X, Y)
+print("Vectores de Soporte (linear):\n", modelo_lineal.support_vectors_)
+print("Predicción [5,4] (linear):", modelo_lineal.predict(nuevo_punto)[0])
+
+
+modelo_rbf = SVC(kernel='rbf')
+modelo_rbf.fit(X, Y)
+print("Vectores de Soporte (rbf):\n", modelo_rbf.support_vectors_)
+print("Predicción [5,4] (rbf):", modelo_rbf.predict(nuevo_punto)[0])
