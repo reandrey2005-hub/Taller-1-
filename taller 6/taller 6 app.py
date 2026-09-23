0730f2683920a4e2d31a4d7d8671374276aa65b4
reandrey2005-hub <reandrey.2005@gmail.com>
2026-09-23 16:07:54 +0000

    cambio

--- taller 6/taller 6 app.py
+++ taller 6/taller 6 app.py
@@ -1,24 +1,31 @@
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 
-# 1. Dataset de Entrenamiento: [Característica 1, Característica 2]
+
 X_entrenamiento = np.array([
-    [20, 30],  # Punto A
-    [40, 50],  # Punto B
-    [35, 45]   # Punto C
+    [20, 30, 0],
+    [22, 32, 0],
+    [25, 35, 1],
+    [28, 38, 0],
+    [23, 28, 0],
+    [26, 30, 1],
+    [40, 50, 2],
+    [35, 45, 1],
+    [45, 60, 3],
+    [38, 52, 2],
+    [42, 55, 2],
+    [50, 65, 3]
 ])
 
-# Etiquetas: 0 = NO COMPRA, 1 = COMPRA
-Y_entrenamiento = np.array([0, 1, 1])
 
-# 2. Instanciar el modelo con K = 3
-modelo_knn = KNeighborsClassifier(n_neighbors=3)
+Y_entrenamiento = np.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
 
-# 3. "Entrenar" (Memorizar los datos)
-modelo_knn.fit(X_entrenamiento, Y_entrenamiento)
 
-# 4. Predecir un nuevo punto
-nuevo_cliente = np.array([[30, 40]])
-prediccion = modelo_knn.predict(nuevo_cliente)
+nuevo_cliente = np.array([[30, 40, 1]])
 
-print("Clase predicha:", prediccion[0])
+
+for k in [1, 5]:
+    modelo_knn = KNeighborsClassifier(n_neighbors=k)
+    modelo_knn.fit(X_entrenamiento, Y_entrenamiento)
+    prediccion = modelo_knn.predict(nuevo_cliente)
+    print(f"K={k} -> Clase predicha:", prediccion[0])
