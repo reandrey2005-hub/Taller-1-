409b3c8d99116520cde1159dac113bacc49609f0
reandrey2005-hub <reandrey.2005@gmail.com>
2026-09-23 16:45:47 +0000

    cambio taller 9

--- taller 9/taller9 app.py
+++ taller 9/taller9 app.py
@@ -1,24 +1,24 @@
 import numpy as np
 
-
 def sigmoide(x):
     return 1 / (1 + np.exp(-x))
 
 
-X = np.array([0.5, 0.8, 0.2])
-
+X = np.array([[0.5, 0.8, 0.2], [0.1, 0.9, 0.9]])
 
 W1 = np.array([
     [0.1,  0.2, -0.3,  0.4],
     [-0.5, 0.6,  0.7, -0.8],
     [0.9, -0.1,  0.2,  0.3]
 ])
-b1 = np.array([0.1, -0.2, 0.3, -0.4]) # 4 Sesgos
+b1 = np.array([0.1, -0.2, 0.3, -0.4])
 
 
 Z1 = np.dot(X, W1) + b1
-A1 = sigmoide(Z1) # Salida de la capa oculta
+A1 = sigmoide(Z1)
 
+print("Z1 (valores puros):\n", np.round(Z1, 4))               
+print("A1 (después de sigmoide):\n", np.round(A1, 4))
 
 W2 = np.array([0.5, -0.6, 0.7, 0.8])
 b2 = np.array([-0.1])
@@ -27,4 +27,4 @@ b2 = np.array([-0.1])
 Z2 = np.dot(A1, W2) + b2
 Salida_Final = sigmoide(Z2)
 
-print("Predicción de la Red (Probabilidad):", np.round(Salida_Final[0], 4))
+print("Predicción de la Red (Probabilidad):", np.round(Salida_Final, 4))  
