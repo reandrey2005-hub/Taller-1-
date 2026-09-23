89490c84ed86be990e9981c6622e7ba0859cfc17
reandrey2005-hub <reandrey.2005@gmail.com>
2026-09-23 16:36:16 +0000

    cambio taller8

--- taller 8/taller 8 app.py
+++ taller 8/taller 8 app.py
@@ -1,28 +1,20 @@
 import numpy as np
 
-# 1. Definir la Función de Activación (Escalón)
 def funcion_escalon(z):
     if z >= 0:
         return 1
     else:
         return 0
 
-# 2. Definir la Estructura de la Neurona
 def perceptron(X, W, b):
-    # Producto punto (Combinación lineal)
-    # Equivalente a: (X[0]*W[0]) + (X[1]*W[1]) ...
     Z = np.dot(X, W) + b
+    return funcion_escalon(Z)
 
-    # Activación
-    salida = funcion_escalon(Z)
-    return salida
 
-# 3. Datos del problema (Compuerta Lógica AND)
-# El AND solo da 1 si ambas entradas son 1.
-entradas = np.array([1, 1])  # Vector X
-pesos = np.array([0.5, 0.5]) # Vector W
-sesgo = -0.8                 # Constante b
+pesos = np.array([0.5, 0.5]) # Vector W (se mantiene)
+sesgo = -0.2                 # CAMBIO: antes -0.8 (AND)
 
-# 4. Inferencia (Forward pass)
-resultado = perceptron(entradas, pesos, sesgo)
-print("El Perceptrón disparó el valor:", resultado)
+
+for entradas in [[0, 0], [0, 1], [1, 0], [1, 1]]:
+    resultado = perceptron(np.array(entradas), pesos, sesgo)
+    print(f"Entrada {entradas} -> {resultado}")
